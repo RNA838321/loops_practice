166d2faa0a8ee03fced0ca2246f1a57e8cd09fbb
RNA838321 <rodneyakong@gmail.com>
2022-12-11 09:03:57 -0800

    writing a string into a file using fputs function

---
+++ files/file03.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * write a string into a file using the fputs function
+ *
+ * Author:Akong,Rodney
+ * Date:12/11/2022
+ * Time:5:45 pm
+ *
+ */
+int main()
+{
+	FILE *ptr;
+	char string[20];
+	printf("please enter a string of 20 characters\n");
+	fgets(string,20,stdin);
+
+	ptr = fopen("secondfile.txt","w");
+
+	fputs(string,ptr);
+
+	return (0);
+}
